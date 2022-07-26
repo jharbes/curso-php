4ec4813afbabb5949316039de9782dfaf235680f
Jorge Harbes <jharbes@hotmail.com>
2022-07-26 13:38:34 -0300

    aula04

---
+++ aula04/tipos.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+<div>
+    <h1>Testando PHP</h1>
+    <?php
+        $n=4;
+        $no="Gustavo";
+        $n=4.5;
+        $idade=36;
+        echo $no," ",$n,"<br>";
+        echo $no." tem ".$idade." anos.";
+        echo "$no tem $idade anos.";
+    ?>
+</div>
+
+</body>
+</html>
