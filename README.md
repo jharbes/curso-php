3b2b1e7724f30a1cb1a38216eac5f5bfaee624ce
Jorge Harbes <jharbes@hotmail.com>
2022-07-26 08:25:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# curso-php
+ Materiais dos cursos de PHP do CFBCursos e CursoemVideo
---
+++ olamundo/ola.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro exemplo PHP</title>
+    <style>
+        h1{
+            font-weight: bolder;
+            font-style: italic;
+            text-decoration: underline;
+        }
+        h2 {
+            color: #7f6363;
+        }
+    </style>
+</head>
+<body>
+<h1>Testando PHP</h1>
+<?php
+    echo "<h2>Olá <br> Mundo!</h2>";
+    echo "testando";
+?>
+
+</body>
+</html>
